cf61790661ca5b62ddf41a2b3625ca2031db42d6
amitnos123 <sami.nosatzki@gmail.com>
2019-08-25 20:21:00 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,98 @@
+//Loading Libs
+const Discord = require('discord.js');
+//const fs = require('fs');
+const client = new Discord.Client();
+
+//Constants
+const token = 'NjE1MTg3NzQ3Mjc5NDcwNjA0.XWKbyQ.CERp9ImWnWlExcNZQykTcJDWffw';
+const PREFIX = '!';
+const JSON_PATH = './json_files/';
+const MAX_DELETE_ROWS = 100;
+
+//JSON loading
+const messages = require(JSON_PATH + 'messages.json');
+
+//Connecting to server
+client.login(token);
+
+//Functions
+function logMessage(debugLvl, logMessage) {
+    switch (debugLvl) {
+        case 'debug':
+            debugLvl = 'DEBUG';
+            break
+        case 'error':
+            debugLvl = 'ERROR';
+            break
+        default:
+            debugLvl = 'INFO';
+            break
+    }
+    console.log(debugLvl + ':' + logMessage);
+}
+
+function isInteger(str) {
+    let patt = new RegExp('^([0-9]+)$');
+    return patt.test(str);
+}
+
+function deleteMessages(message, NumOfMessages) {
+    if (isInteger(NumOfMessages)) {
+        if (parseInt(NumOfMessages) <= MAX_DELETE_ROWS) {
+            message.channel.bulkDelete(NumOfMessages);
+            logMessage('debug', 'Deleted ' + NumOfMessages + ' Messages');
+        } else {
+            message.reply('Max delete row allowed is ' + MAX_DELETE_ROWS);
+        }
+    } else {
+        logMessage('debug', NumOfMessages + ' isn\'t a integer');
+        message.reply('\'' + NumOfMessages + '\' isn\'t a integer');
+    }
+}
+
+function channelLink(channelIdStr) {
+    let str = '<#' + channelIdStr + '>';
+    return str;
+}
+
+//Events
+client.on('ready', function() {
+    console.log('-------------------------------');
+    console.log('Start');
+});
+
+client.on('message', function (message) {
+    if (message.content[0] === PREFIX && !message.author.bot) {
+        console.log('-------------------------------');
+        logMessage('debug', 'On Message Start');
+        console.log('message content: ' + message.content);
+        let arg = message.content.substring(PREFIX.length).split(" ");
+        switch (arg[0]) {
+            case 'ping':
+                message.channel.send('pong');
+                break;
+            case 'delete':
+                if (arg[1]) {
+                    deleteMessages(message, arg[1]);
+                } else {
+                    message.reply('Error, missing arguments for function');
+                    return;
+                }
+                break;
+            case 'testInt':
+                message.channel.send(isInteger(arg[1]));
+                break;
+            case 'testLink':
+                message.channel.send(channelLink("615195381285912600"));
+                break;
+            default:
+                message.channel.send('Error 404');
+                break;
+        }
+        console.log('DEBUG:' + 'On message End');
+    }
+});
+
+client.on("guildMemberAdd", member => {
+    client.channels.get(messages.WELCOME.CHANNEL_ID).send(messages.WELCOME.MESSAGE).catch(console.error);
+});
