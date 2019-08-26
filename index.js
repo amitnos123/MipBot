b92d1c13114dc5d0bc62d8c0b47a87b66bae3a64
amitnos123 <sami.nosatzki@gmail.com>
2019-08-26 18:15:33 +0300

    changed some things

--- index.js
+++ index.js
@@ -11,49 +11,10 @@ const MAX_DELETE_ROWS = 100;
 //JSON loading
 const messages = require(JSON_PATH + 'messages.json');
 const config = require(JSON_PATH + 'config.json');
+const channels = require(JSON_PATH + 'channels.json');
 
 //Connecting to server
 client.login(config.token);
-
-//Functions
-function logMessage(debugLvl, logMessage) {
-    switch (debugLvl) {
-        case 'debug':
-            debugLvl = 'DEBUG';
-            break
-        case 'error':
-            debugLvl = 'ERROR';
-            break
-        default:
-            debugLvl = 'INFO';
-            break
-    }
-    console.log(debugLvl + ':' + logMessage);
-}
-
-function isInteger(str) {
-    let patt = new RegExp('^([0-9]+)$');
-    return patt.test(str);
-}
-
-function deleteMessages(message, NumOfMessages) {
-    if (isInteger(NumOfMessages)) {
-        if (parseInt(NumOfMessages) <= MAX_DELETE_ROWS) {
-            message.channel.bulkDelete(NumOfMessages);
-            logMessage('debug', 'Deleted ' + NumOfMessages + ' Messages');
-        } else {
-            message.reply('Max delete row allowed is ' + MAX_DELETE_ROWS);
-        }
-    } else {
-        logMessage('debug', NumOfMessages + ' isn\'t a integer');
-        message.reply('\'' + NumOfMessages + '\' isn\'t a integer');
-    }
-}
-
-function channelLink(channelIdStr) {
-    let str = '<#' + channelIdStr + '>';
-    return str;
-}
 
 //Events
 client.on('ready', function() {
@@ -96,3 +57,48 @@ client.on('message', function (message) {
 client.on("guildMemberAdd", member => {
     client.channels.get(messages.WELCOME.CHANNEL_ID).send(messages.WELCOME.MESSAGE).catch(console.error);
 });
+
+
+//Functions
+function logMessage(debugLvl, logMessage) {
+    switch (debugLvl) {
+        case 'debug':
+            debugLvl = 'DEBUG';
+            break
+        case 'error':
+            debugLvl = 'ERROR';
+            break
+        default:
+            debugLvl = 'INFO';
+            break
+    }
+    console.log(debugLvl + ':' + logMessage);
+}
+
+function isInteger(str) {
+    let patt = new RegExp('^([0-9]+)$');
+    return patt.test(str);
+}
+
+function deleteMessages(message, NumOfMessages) {
+    if (isInteger(NumOfMessages)) {
+        if (parseInt(NumOfMessages) <= MAX_DELETE_ROWS) {
+            message.channel.bulkDelete(NumOfMessages);
+            logMessage('debug', 'Deleted ' + NumOfMessages + ' Messages');
+        } else {
+            message.reply('Max delete row allowed is ' + MAX_DELETE_ROWS);
+        }
+    } else {
+        logMessage('debug', NumOfMessages + ' isn\'t a integer');
+        message.reply('\'' + NumOfMessages + '\' isn\'t a integer');
+    }
+}
+
+function channelLink(channelIdStr) {
+    let str = '<#' + channelIdStr + '>';
+    return str;
+}
+
+function writeToChannel() {
+	
+}
