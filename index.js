217fa9cf56ed0b6a81b9aadf8fc865157a5b90ae
amitnos123 <sami.nosatzki@gmail.com>
2019-08-31 10:56:24 +0300

    fixed bug with the welcome message on member joining the server

--- index.js
+++ index.js
@@ -43,46 +43,69 @@ client.on('ready', function() {
 
 client.on('message', function (message) {
     let messageSend = '';
+    let memberArr = [];
     if (message.content[0] === PREFIX && !message.author.bot) {
         logMessage('debug', 'On Message Start');
-        let arg = message.content.substring(PREFIX.length).split(" ");
-        switch (arg[0]) {
-            case 'ping':
-                message.channel.send('pong');
-                break;
-            case 'delete':
-                if (arg[1]) {
-                    deleteMessages(message, arg[1]);
-                } else {
-                    message.reply('Error, missing arguments for function');
-                    return;
-                }
-                break;
-            case 'testMessage':
-                messageSend = replaceWith(messages['WELCOME']['MESSAGE'], '<', '>', convertChannelLink, channels['text']);
-                let memberArr = [];
-				
-				if(arg[1] != undefined) { memberArr['memberName'] = arg[1];}
-				else { memberArr['memberName'] = '615187747279470604'; }
-				
-				messageSend = replaceWith(messageSend, '{', '}', convertMemberLink, memberArr);
-				message.channel.send(messageSend);
-                break;
-            case 'testGetChannelId':
-                messageSend = channels['text'][arg[1]];
-                message.channel.send(messageSend);
-                break;
-			case 'man':
-                if(man[arg[1]] != undefined) {
-					messageSend = man[arg[1]];
-					message.channel.send(messageSend);
-				} else {
-					message.channel.send('Error 404: Function doesn\'t exist in man');
-				}
-                break;
-            default:
-                message.channel.send('Error 404: Function doesn\'t exist');
-                break;
+        try 
+        {
+            let arg = message.content.substring(PREFIX.length).split(" ");
+            switch (arg[0]) {
+                case 'ping':
+                    message.channel.send('pong');
+                    break;
+                case 'delete':
+                    if (arg[1]) {
+                        deleteMessages(message, arg[1]);
+                    } else {
+                        message.reply('Error, missing arguments for function');
+                        return;
+                    }
+                    break;
+                case 'testMessage':
+                    messageSend = replaceWith(messages['WELCOME']['MESSAGE'], '<', '>', convertChannelLink, channels['text']);
+                    
+                    if(arg[1] != undefined) { memberArr['memberName'] = arg[1];}
+                    else { memberArr['memberName'] = '615187747279470604'; }
+                    
+                    messageSend = replaceWith(messageSend, '{', '}', convertMemberLink, memberArr);
+                    message.channel.send(messageSend);
+                    break;
+                case 'testMessageGeneral':
+                        messageSend = replaceWith(messages['WELCOME']['MESSAGE'], '<', '>', convertChannelLink, channels['text']);
+                        memberArr['memberName'] = 109982657667944448;
+                        messageSend = replaceWith(messageSend, '{', '}', convertMemberLink, memberArr);
+                        client.channels.get(channels['text'][messages.WELCOME.CHANNEL]).send(messageSend).catch(console.error);
+                        break;
+                case 'testGetChannelId':
+                    messageSend = channels[arg[1]][arg[2]];
+                    message.channel.send(messageSend);
+                    break;
+                case 'testGetWelcomeChannelId':
+                        //messageSend = channels['text'][messages.WELCOME.CHANNEL];
+                        //message.channel.send(messageSend);
+                        //let ch = client.channels.get(channels['text'][messages.WELCOME.CHANNEL]);
+                        //logMessage('debug', ch);
+                        messageSend = 'TEST WORKS';
+                        client.channels.get(channels['text'][messages.WELCOME.CHANNEL]).send(messageSend).catch(console.error);
+                        break;
+                case 'testGetChannelIdByMessage':
+                    messageSend = channels['text'][arg[1]];
+                    message.channel.send(messageSend);
+                    break;
+                case 'man':
+                    if(man[arg[1]] != undefined) {
+                        messageSend = man[arg[1]];
+                        message.channel.send(messageSend);
+                    } else {
+                        message.channel.send('Error 404: Function doesn\'t exist in man');
+                    }
+                    break;
+                default:
+                    message.channel.send('Error 404: Function doesn\'t exist');
+                    break;
+            }
+        } catch(err) {
+            logMessage('error', err);
         }
 
         logMessage('debug', 'On message End');
@@ -90,11 +113,12 @@ client.on('message', function (message) {
 });
 
 client.on("guildMemberAdd", member => {
-    messageSend = replaceWith(messages['WELCOME']['MESSAGE'], '<', '>', convertChannelLink, channels['text']);
+    let chId = channels['text'][messages.WELCOME.CHANNEL];
     let memberArr = [];
     memberArr['memberName'] = member.id;
+    messageSend = replaceWith(messages['WELCOME']['MESSAGE'], '<', '>', convertChannelLink, channels['text']);
     messageSend = replaceWith(messageSend, '{', '}', convertMemberLink, memberArr);
-    client.channels.get(messages.WELCOME.CHANNEL_ID).send(messages.WELCOME.MESSAGE).catch(console.error);
+    client.channels.get(chId).send(messageSend).catch(console.error);
 });
 
 
@@ -157,6 +181,13 @@ function convertMemberLink(MemberIdStr) {
     return str;
 }
 
+/*
+messageStr - String that is the message
+startChar - the first char, that will be a flag for where are the substrings start
+endChar - the first char, that will be a flag for where are the substrings end
+convertFunc - Function which convert the string to Another string
+strArr - Array of the substring to put in, where the keys are the substring in the original messageStr
+*/
 function replaceWith(messageStr, startChar, endChar, convertFunc, strArr) {
     let startSubString = 1;
     let endSubString = 0;
